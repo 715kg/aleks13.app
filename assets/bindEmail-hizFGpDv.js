1594f4babac44842dc2ded7bd9b8c549a3e79a50
715kg <info-715kg@yandex.ru>
2026-09-17 12:19:58 +0500

    Updates

--- assets/bindEmail-hizFGpDv.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./index-BrQ8cMOW.js";var t=e(`bindEmail`,{state:()=>({isOpen:!1,autoDismissed:!1}),actions:{open(){this.isOpen=!0},close(){this.isOpen=!1},dismissAuto(){this.autoDismissed=!0,this.isOpen=!1}}});export{t};
